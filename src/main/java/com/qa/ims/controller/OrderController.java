c078875afdb2522aada1b740efbf7a6437b2c898
tufayel98 <tufayel98@live.co.uk>
2022-07-28 02:01:48 +0100

    Updates on orders

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -32,9 +32,9 @@ public class OrderController implements CrudController<Order> {
 
 	@Override
 	public Order create() {
-		Logger.info("Enter a order ID");
-		String customerId = utils.getString();
-		Order order = orderDAO.create(new Order(customerId));
+		Logger.info("Enter a order item");
+		String customer_Id = utils.getString();
+		Order order = orderDAO.create(new Order(customer_Id));
 		Logger.info(" created");
 		return order;
 	}
@@ -44,8 +44,8 @@ public class OrderController implements CrudController<Order> {
 		Logger.info("Enter the id of the orders you would like to update");
 		Long id = utils.getLong();
 		Logger.info("Please enter a customer id");
-		String customerId = utils.getString();
-		Order order = orderDAO.update(new Order(id, customerId));
+		String customer_id = utils.getString();
+		Order order = orderDAO.update(new Order(id, customer_id));
 		Logger.info("Customer Updated");
 		return order;
 	}
