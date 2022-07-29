961b8ca398387a1e68a384dcf0685f9f2a0f5d99
tufayel98 <tufayel98@live.co.uk>
2022-07-29 10:57:21 +0100

    Testing

--- src/test/java/com/qa/ims/controllers/OrderControllerTest.java
+++ src/test/java/com/qa/ims/controllers/OrderControllerTest.java
@@ -1,14 +1,26 @@
 package com.qa.ims.controllers;
 
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import org.junit.Test;
+import com.qa.ims.controller.OrderController;
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.utils.Utils;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OrderControllerTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	@Mock
+	private Utils utils;
+
+	@Mock
+	private OrderDAO dao;
+
+	@InjectMocks
+	private OrderController controller;
 
 }
+
+
--- src/test/java/com/qa/ims/persistence/dao/OrderDOATest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDOATest.java
@@ -1,6 +1,6 @@
 package com.qa.ims.persistence.dao;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,54 +8,53 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DBUtils;
 
 public class OrderDOATest {
+    
+    private final OrderDAO DAO = new OrderDAO();
 
-	private final OrderDAO DAO = new OrderDAO();
+    @Before
+    public void setup() {
+        DBUtils.connect();
+        DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+    }
 
-	@Before
-	public void setup() {
-		DBUtils.connect();
-		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
-	}
+    @Test
+    public void testCreate() {
+        final Order created = new Order(1l);
+        assertEquals(created, DAO.create(created));
+    }
 
-	@Test
-	public void testCreate() {
-		final Order created = new Order(2L);
-		assertEquals(created, DAO.create(created));
-	}
+    @Test
+    public void testReadAll() {
+        List<Order> expected = new ArrayList<>();
+        expected.add(new Order(1l));
+        assertEquals(expected, DAO.readAll());
+    }
+//
+//    @Test
+//    public void testReadLatest() {
+//        assertEquals(new Order(1l), DAO.readLatest());
+//    }
 
-	@Test
-	public void testReadAll() {
-		List<Customer> expected = new ArrayList<>();
-		expected.add(new Customer(1L, "jordan", "harrison"));
-		assertEquals(expected, DAO.readAll());
-	}
+    @Test
+    public void testRead() {
+        final long ID = 1L;
+        assertEquals(new Order(ID, 1l), DAO.read(ID));
+    }
 
-	@Test
-	public void testReadLatest() {
-		assertEquals(new order(1L, "Rolex", 5000.00f), DAO.readLatest());
-	}
+    @Test
+    public void testUpdate() {
+        final Order updated = new Order(1l);
+        assertEquals(updated, DAO.update(updated));
 
-	@Test
-	public void testRead() {
-		final long ID = 1L;
-		assertEquals(new Customer(ID, "jordan", "harrison"), DAO.read(ID));
-	}
+    }
 
-	@Test
-	public void testUpdate() {
-		final Order updated = new Order(2L,( "Rolex", 5000.00F);
-		assertEquals(updated, DAO.update(updated));
-
-	}
-
-	@Test
-	public void testDelete() {
-		assertEquals(1, DAO.delete(1));
+    @Test
+    public void testDelete() {
+        assertEquals(1, DAO.delete(1));
 	}
 
 }
